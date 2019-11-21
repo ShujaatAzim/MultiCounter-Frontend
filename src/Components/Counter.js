e12077450285267bf97e5e37903b08863fd721be
Shujaat Azim <shujaat.azim@gmail.com>
2019-11-20 22:06:29 -0500

    added functionality to the increase and decrease buttons using hooks instead of class state

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = props => {
- 
+
+  const [count, setCount] = useState(props.counterObj.amount)
+  
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div>
       <hr />
       <h4>Counter #{props.counterObj.number}</h4>
-      <h5>{props.counterObj.name}: {props.counterObj.amount} <button>+</button><button>-</button></h5>
+      <h5>{props.counterObj.name}: {count} <button onClick={increase}>+</button><button onClick={decrease}>-</button></h5>
     </div>
     )
 
