43f6fca5cdc88bd7088eec735761c63fe3041bb4
Shujaat Azim <shujaat.azim@gmail.com>
2020-01-06 23:17:28 -0500

    built out tag component sample, changed add tag form to a react-bootstrap form

--- src/Components/AddTagForm.js
+++ src/Components/AddTagForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const AddTagForm = props => {
 
@@ -6,18 +8,18 @@ const AddTagForm = props => {
 
   const addNewTag = event => {
     event.preventDefault()
-    const newTag = {name: newTagName}
+    const newTag = {"name": newTagName}
     props.postTag(newTag)
   }
 
   return (
     <div>
       <h3>Add a Tag!</h3>
-      <form onSubmit={addNewTag}>
-        <label>New Tag Name: </label>
-        <input type="text" name="name" value={newTagName} onChange={(e) => setNewTagName(e.target.value)} />
-        <input type="submit" />
-      </form>
+      <Form onSubmit={addNewTag}>
+        <Form.Label>New Tag Name: </Form.Label>
+        <Form.Control type="text" name="name" value={newTagName} onChange={(e) => setNewTagName(e.target.value)} />
+        <Button type="submit" value="done">Submit</Button>
+      </Form>
     </div>
   )
 }
---
+++ src/Components/Tag.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Tag = () => {
+
+  const [allTags] = useState([{name: dingo}])
+
+  return (
+    <h1>Tag Placeholder</h1>
+  )
+}
+
+export default Tag
